2cca201219f63878d220ae1f58b4273cc2e7c161
gr8plum <gr8plum@gmail.com>
2019-09-09 13:07:18 -0600

    Updates

---
+++ precache-manifest.01fbccef595285789e4809547d015424.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd1e3e32a63955adc855d4f4ae24db1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0143fc86be24eaf521d8",
+    "url": "/monsters-rolodex/static/css/main.7b8a0a5e.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "0143fc86be24eaf521d8",
+    "url": "/monsters-rolodex/static/js/main.81cba217.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
